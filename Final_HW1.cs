cf41f277268b67a1ea3398ab7cb5734cbc64b5ed
ASFedotov <asfedotov1988as@gmail.com>
2023-01-25 20:09:26 +0500

    add changes

--- Final_HW1.cs
+++ Final_HW1.cs
@@ -1,17 +1,28 @@
-﻿void MaxNum3(string[] array){
+﻿// Написать программу которая из имеющегося массива строк формирует массив из 
+// строк, длина которых мень ше либо равна 3 символа. Первоначальный массив 
+// можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При 
+// решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
 
-    for (int i = 0; i < array.Length; i++)
+string [] ArrayString(int num){
+    string [] array = new string [num];
+    for (int i = 0; i < num; i++)
     {
-        if (array[i].Length <= 3)
-            Console.Write($"{array[i]}  ");     
-    }
-}
+        Console.WriteLine($"Введите {i+1} строку: ");
+        array[i] = Console.ReadLine();
+        while (array[i] == "")
+        {
+            Console.WriteLine("Введите корректное значение: ");
+            Console.WriteLine($"Введите {i+1} строку: ");
+            array[i] = Console.ReadLine();
+        }
 
-System.Console.WriteLine("Введите желаемое количество чисел: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-string [] zero = ArrayString(num);
-ShowArray(zero);
-Console.WriteLine();
-Console.WriteLine($"Строки, длина которых меньше либо равна 3 символам!!! ");
-MaxNum3(zero);
+    }
+    return array;
+﻿void ShowArray(string [] array){
+    
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($" {array[i]}");
+    }
+    Console.WriteLine();
+}
